40d8791afca6823e754781cde0d261a96b446633
Gabriel Coimbra De Oliveira Da Silva <gabao.coimbra@gmail.com>
2022-05-10 11:52:12 -0300

    Criando Migrations

---
+++ api-nokkamisas/database/migrations/1652193956670_products.ts
@@ -0,0 +1,26 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Products extends BaseSchema {
+  protected tableName = 'products'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('categoria').notNullable()
+      table.string('nomeProduto').notNullable()
+      table.string('descricaoProduto').notNullable()
+      table.string('valor').notNullable()
+      table.string('imagem').notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
