46f153c3b87c0beea2bf60beb9062ae7f0740f73
Gabriel Coimbra De Oliveira Da Silva <gabao.coimbra@gmail.com>
2022-05-11 11:06:00 -0300

    ajustando rotas individuais

---
+++ api-nokkamisas/start/routes/usuarios.ts
@@ -0,0 +1,8 @@
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.group(() => {
+  Route.get('/usuarios', 'UsersController.getAll')
+  Route.get('/usuarios/:id', 'UsersController.get')
+  Route.post('/usuarios/', 'UsersController.post')
+  Route.put('/usuarios/:id', 'UsersController.put')
+})
